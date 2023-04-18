21e2512aa3f42b8962a10e7160d7188c341664c4
Matthew <macrossan17@gmail.com>
2023-04-17 23:17:14 -0700

    Added deck edit page.

--- src/pages/EditDeck/displayImage.js
+++ src/pages/EditDeck/displayImage.js
@@ -1,3 +1,3 @@
-export default displayImage() {
-    
+export default function displayImage() {
+
 }
--- src/pages/EditDeck/index.jsx
+++ src/pages/EditDeck/index.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AuthContext from '../../context/AuthContext';
 import { useParams } from 'react-router-dom';
+import useFetchData from '../../hooks/useFetchData';
+import { GET_DECKS_ENDPOINT, UPDATE_DECK_ENDPOINT, DELETE_DECK_ENDPOINT } from '../../utils/api'
 
 import styles from '../../assets/styles.module.css';
 
@@ -22,79 +25,221 @@ import styles from '../../assets/styles.module.css';
 //       (5) save button at the bottom. 
 //       (6) notify user of the update
 
-function InputCard() {
+function InputCard({ setModifiedCards }) {    
+    const [front, setFront] = useState('');
+    const [back, setBack] = useState('');
+
+    const handleAdd = (e) => {
+        console.log('clicking')
+        const newCard = {
+            front,
+            back,
+        }
+        setModifiedCards(prevState => {
+            return [newCard,...prevState]
+        })
+        setFront('')
+        setBack('')
+    }
     return(
         <div>
             <div>
                 <label htmlFor='front'>front</label>
-                <input type='text'/>
+                <input type='text' value={front} onChange={(e) => {
+                    setFront(e.target.value);
+                }}/>
             </div>
             <div>
-                <label htmlFor='front'>back</label>
-                <input type='text'/>
+                <label htmlFor='back'>back</label>
+                <input type='text' value={back} onChange={(e) => {
+                    setBack(e.target.value);
+                }}/>
             </div>
             <div>
-                <button>add</button>
+                <button onClick={handleAdd}>add</button>
             </div>
         </div>
     )
 }
 
-function Card() {
+function Card({id, front, back, setModifiedCards}) {
+    const [isEdit, setIsEdit] = useState(false);
+    const [frontField, setFrontField] = useState(front.slice());
+    const [backField, setBackField] = useState(back.slice());
+    const handleDelete = (e) => {
+        setModifiedCards(prevState => {
+            let updated = prevState.filter((card) => card._id !== id)
+            return [...updated]
+        })
+    }
+    const handleEdit = (e) => {
+        setFrontField(front)
+        setBackField(back)
+        setIsEdit(!isEdit)
+    }
+    const handleUpdate = (e) => {
+        setModifiedCards(prevState => {
+
+            let card = prevState.reduce((current, elm) => (elm._id === id) ? elm : current);
+            let temp = {...card}
+            temp.front = frontField;
+            temp.back = backField;
+
+            return [temp, ...prevState.filter((card) => card._id !== id)]
+        })
+        setIsEdit(!isEdit)
+    }
+    const handleCancel = (e) => {
+        setIsEdit(!isEdit)
+
+    }
+    let cardComponent = (!isEdit) ? 
+            <div>
+                <div>
+                    <span>Front:</span><span> {front}</span>
+                </div>
+                
+                <div>
+                    <span>back:</span><span> {back}</span>
+                </div>
+
+                <div>
+                    <button onClick={handleDelete}>Delete</button>
+                    <button onClick={handleEdit}>edit</button>
+                </div>
+            </div>
+            :
+            <div>
+                <label>front:</label>
+                <input value={frontField} onChange={(e) => {setFrontField(e.target.value)}}/>
+                <br />
+                <label>back:</label>
+                <input value={backField} onChange={(e) => {setBackField(e.target.value)}}/>
+                <div>
+                    <button onClick={handleUpdate}>update</button>
+                    <button onClick={handleCancel}>cancel</button>
+                </div>
+            </div>
     return (
         <div>
-            <div>
-                <span>Front:</span><span> Definition goes here</span>
-            </div>
-            
-            <div>
-                <span>back:</span><span> meaning goes here</span>
-            </div>
-
-            <div>
-                <button>Delete</button>
-                <button>edit</button>
-            </div>
+            {cardComponent}
         </div>
     )
 }
-
 
 export default function EditDeckPage() {
     const authContext = useContext(AuthContext)
+    const navigate = useNavigate();
     const { id } = useParams();
 
-    const [cards, setCards] = useState([])
+    const url =  `${process.env.REACT_APP_SERVER_BASE_URL}${GET_DECKS_ENDPOINT(authContext.auth.userId)}`;
+    const options = { method: 'GET', headers: {'Authorization': `Bearer ${authContext.auth.token}`}}
+    const deck = useFetchData(url, options)?.reduce((current, elm) => (elm._id === id) ? elm : current);
+    const [cards, setCards] = useState([]);
+    const [modifiedCards, setModifiedCards] = useState([]);
+
+    // input fields
+    const [deckName, setDeckName] = useState('')
+    const [newCardCount, setNewCardCount] = useState(0);
+    const [reviewedCardCount, setReviewedCardCount] = useState(0);
+    const [deckImage, setDeckImage] = useState('');
 
     useEffect(() => {
-        // first check local storage
-        let decks = JSON.parse(window.localStorage.getItem('decks'));
-        let deck = {}
-        if(decks.length == 0) {  // if local storage is empty, make request
-            fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/deck/${authContext.auth.userId}`, {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Bearer ${authContext.auth.token}`
-                }
-            })
-                .then(res => res.json())
-                .then(data => {
-                    deck = data.details.reduce((current, card) => (card._id === id) ? card : current)
-                    setCards(deck.cards)
-                })
-        }
-        else {
-            deck = decks.reduce((current, card) => (card._id === id) ? card : current)
-            setCards(deck.cards)
-        }
+        setDeckName(deck?.deckName)
+        setNewCardCount(deck?.deckSettings?.newCards)
+        setReviewedCardCount(deck?.deckSettings?.reviewCards)
+        setCards(deck?.cards);
+        setModifiedCards(deck?.cards);
+    }, [deck])
 
-    }, [])
+    let CardList = (modifiedCards)
+        ? modifiedCards?.map(card => {
+                return <Card id={card._id} front={card.front} back={card.back} setModifiedCards={setModifiedCards} key={card._id} />
+            })
+        : [];
+
+    let DeckImageComponent = (deckImage) ? <img src={deckImage}/> : null
+
+    const handleSaveDeck = async (e) => {
+        const updatedDeck = {
+            deckName,
+            deckImage: (deckImage) ? deckImage : deck?.deckImage,
+            cards: modifiedCards,
+            deckSettings: {
+                reviewCards: reviewedCardCount,
+                newCards: newCardCount
+            }
+        }
+        const res = await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}${UPDATE_DECK_ENDPOINT(id)}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${authContext.auth.token}`
+            },
+            body: JSON.stringify({details: updatedDeck})
+        });
+        const data = await res.json();
+        navigate('/');
+    }
+    const handleDeleteDeck = async () => {
+        // make delete request 
+        await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}${DELETE_DECK_ENDPOINT(id)}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${authContext.auth.token}`,
+            }
+        })
+        navigate('/');
+    }
+    const handleCancel = () => {
+        navigate('/');
+    }
     return(
         <section className={styles['container']}>
             <h1>Edit Deck</h1>
+            <button onClick={handleSaveDeck}>Save</button>
+            <button onClick={handleCancel}>Cancel</button>
+            <button onClick={handleDeleteDeck}>Delete</button>
             <div>
-                <InputCard />
-                <Card />
+                <div>
+                    {
+                        (deck?.deckImage)
+                        ? <img src={deck?.deckImage} alt="deck image"/>
+                        : ""
+                    }
+                    
+                </div>
+                <div>
+                    <label>Deck Name</label>
+                    <input type="text" value={deckName} onChange={(e) => {
+                        setDeckName(e.target.value);
+                    }}/>
+                </div>
+            </div>
+            <p>Review settings</p>
+            <div>
+                <p>Number of cards added each review session</p>
+                <div><label>new cards:</label> <input type="input" value={newCardCount} onChange={(e) => {
+                    setNewCardCount(e.target.value)
+                }}/></div>
+                <div><label>reviewed cards:</label><input type="input" value={reviewedCardCount} onChange={(e) => {
+                    setReviewedCardCount(e.target.value)
+                }}/></div>
+            </div>
+            <div>
+                <p>update deck image</p>
+                <input type="file" onChange={async (e) => {
+                    const {handleUploadImageEvent} = await import('../../utils/uploadImage')
+                    handleUploadImageEvent(e.target.files, setDeckImage)
+                }}/>
+                <div style={{width: "100px", height: "100px", overflow: "hidden"}}>
+                    {DeckImageComponent}
+                </div>
+            </div>
+            <div>
+                <InputCard  setModifiedCards={setModifiedCards}/>
+                {CardList}
             </div>
         </section>
     )
