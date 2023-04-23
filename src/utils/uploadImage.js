421a747ecdd8e43c9a579cf25e4c931c2c1e4c0a
Matthew <macrossan17@gmail.com>
2023-04-23 00:08:27 -0700

    Modified handleUploadImageEvent function to create unique image file names. This commit updates the function to call setDeckImageName callback function with image name concatenated with a unique identifier created using uuidv4. This ensures each upload to firebase produces a unique file name used for querying.

--- src/utils/uploadImage.js
+++ src/utils/uploadImage.js
@@ -1,7 +1,12 @@
 import Compressor from "compressorjs";
-export function handleUploadImageEvent(upload, setDeckImg) {
+import { v4 as uuidv4 } from 'uuid'
+export function handleUploadImageEvent(upload, setDeckImg, setDeckImageName) {
     let file = upload[0];
-    console.log(file)
+    let fileExt = file.name.split('.').pop();         // get file extension
+    let fileName = file.name.split(`.${fileExt}`)[0]  // get file name
+    fileName = fileName.split(' ').join('');          // remove spaces
+    setDeckImageName(fileName + '_id_' + uuidv4() + '.' + fileExt);  // pass to callback
+
     new Compressor(file, {
         quality: 0.2,
         success(result) {
@@ -12,7 +17,7 @@ export function handleUploadImageEvent(upload, setDeckImg) {
             if (file.type.match(/image.*/)) {
                 const reader = new FileReader();
                 reader.onloadend = handleFile;
-                reader.readAsDataURL(file);
+                return reader.readAsDataURL(file);
             }
         },
         error(err) {
