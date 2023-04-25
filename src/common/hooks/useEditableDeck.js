b57d9fd055db3c393c11a5b40c3aab77b04e63f0
Matthew <macrossan17@gmail.com>
2023-04-24 22:42:23 -0700

    Added common useEditableDeck custom hook for managing editable deck data.\n The hook will replace the state variables in the addDeck page and editDeck page \n to fetch the deck (depending on the parameters), and to manage the data and the changing values.

---
+++ src/common/hooks/useEditableDeck.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+import { GET_DECK_ENDPOINT } from '../../../utils/api'
+
+export default function useEditableDeck(shouldFetchDeck=false, deckId='', headers={}) {
+    const [editableDeck, setEditableDeck] = useState(() => buildDeck())
+
+    useEffect(() => {
+        if(shouldFetchDeck) {
+            
+            (async function() {
+                const fetchedDeck = await fetchDeck(deckId, headers)
+                setEditableDeck(prevState => {
+                    return {
+                        ...prevState,
+                        deckName: fetchedDeck?.deckName,
+                        newCardCount: fetchedDeck?.deckSettings?.newCards,
+                        reviewedCardCount: fetchedDeck?.deckSettings?.reviewCards,
+                        modifiedCards: fetchedDeck?.cards,
+                        deckImage: fetchedDeck?.deckImage
+                    }
+                })
+            })()
+        }
+    }, []);
+    
+    return [editableDeck, setEditableDeck];
+}
+
+async function fetchDeck(deckId, headers) {
+    const url =  `${process.env.REACT_APP_SERVER_BASE_URL}${GET_DECK_ENDPOINT(deckId)}`;
+
+    try {
+        const res = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                ...headers
+            }
+        })
+        const data = await res.json()
+        return (data?.details) ? data.details : data
+    } catch (err) {
+        throw new Error(err)
+    }
+}
+
+function buildDeck(deckName='', newCardCount=0, reviewedCardCount=0, deckImage='', deckImageName='', deckImageFile='', modifiedCards=[]) {
+    return new Object({
+        deckName,
+        newCardCount,
+        reviewedCardCount,
+        deckImage,
+        deckImageName,
+        deckImageFile,
+        modifiedCards,
+    })
+}
