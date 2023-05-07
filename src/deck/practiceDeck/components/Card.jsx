3535494e8c568bf60dbc96349e454164a56dd5be
nassorc <macrossan17@gmail.com>
2023-05-07 13:11:16 -0700

    Fix Practice card component baceFaceVisibililty not working
    When the flashcard flips, both front and back sides of the card are visible. Flashcard component should only display one side, and this fix hides the current side that faces the back.

--- src/deck/practiceDeck/components/Card.jsx
+++ src/deck/practiceDeck/components/Card.jsx
@@ -1,14 +1,39 @@
-import { useState } from 'react';
-import CardFront from './CardFront';
-import CardBack from './CardBack';
+import { useState } from "react";
+import CardFront from "./CardFront";
+import CardBack from "./CardBack";
 
-export default function Card({ cardDetails, gradeCard, handleEditClick, cards, setCards }) {
-    const [showMeaning, setShowMeaning] = useState(false);  // if true, shows back of card
-    let rotateStyle = (showMeaning) ? {transform: 'rotateY(180deg)'} : {}
-    return(
-        <div className='w-full h-48 absolute transition-all duration-500 ease' style={{transformStyle: 'preserve-3d', ...rotateStyle}} >
-            <CardBack cardDetails={cardDetails} gradeCardProp={gradeCard} showMeaningProp={showMeaning} setShowMeaningProp={setShowMeaning} cards={cards} setCards={setCards}/>
-            <CardFront cardDetails={cardDetails} showMeaningProp={showMeaning} setShowMeaningProp={setShowMeaning} handleEditClick={handleEditClick}/>
+export default function Card({
+    cardDetails,
+    gradeCard,
+    handleEditClick,
+    cards,
+    setCards,
+}) {
+    const [showMeaning, setShowMeaning] = useState(false); // if true, shows back of card
+    let rotateStyle = showMeaning ? { transform: "rotateY(180deg)" } : {};
+    return (
+        <div
+            className="w-full h-48 absolute transition-all duration-500 ease"
+            style={{
+                transformStyle: "preserve-3d",
+                backfaceVisibility: "hidden",
+                ...rotateStyle,
+            }}
+        >
+            <CardBack
+                cardDetails={cardDetails}
+                gradeCardProp={gradeCard}
+                showMeaningProp={showMeaning}
+                setShowMeaningProp={setShowMeaning}
+                cards={cards}
+                setCards={setCards}
+            />
+            <CardFront
+                cardDetails={cardDetails}
+                showMeaningProp={showMeaning}
+                setShowMeaningProp={setShowMeaning}
+                handleEditClick={handleEditClick}
+            />
         </div>
-    )
+    );
 }
--- src/deck/practiceDeck/components/CardFront.jsx
+++ src/deck/practiceDeck/components/CardFront.jsx
@@ -1,15 +1,24 @@
-export default function CardFront({ cardDetails, showMeaningProp, setShowMeaningProp, handleEditClick }) {
+export default function CardFront({
+    cardDetails,
+    showMeaningProp,
+    setShowMeaningProp,
+    handleEditClick,
+}) {
     const handleShowClick = (e) => {
         setShowMeaningProp(!showMeaningProp);
-    }
-    return(
-        <div className='p-4 absolute w-full h-full flex flex-col items-center justify-between bg-slate-200 rounded-lg border border-sm border-slate-400' style={{backfaceVisibility: 'hidden'}}>
+    };
+    return (
+        <div className="p-4 absolute w-full h-full flex flex-col items-center justify-between bg-slate-200 rounded-lg border border-sm border-slate-400">
             <div>
-                <div className='m-0 font-bold text-lg'>{cardDetails.front}</div>
+                <div className="m-0 font-bold text-lg">{cardDetails.front}</div>
             </div>
-            <div className='w-full h-0.5 bg-black'></div>
-            <button className='py-2 px-8 w-full border border-slate-400 rounded-lg bg-white' onClick={handleShowClick}>show</button>
-
+            <div className="w-full h-0.5 bg-black"></div>
+            <button
+                className="py-2 px-8 w-full border border-slate-400 rounded-lg bg-white"
+                onClick={handleShowClick}
+            >
+                show
+            </button>
         </div>
     );
 }
