c1ca12dd85fa41269d79764655823c0505949e97
nassorc <macrossan17@gmail.com>
2023-08-18 02:30:53 -0700

    Add mock object to mimic replicate's blip-2 api
    Add class to mimic replicate's blip-2 service. This will be used to develop
    the feature of generating images for flashcards.

---
+++ src/shared/services/replicate.ts
@@ -0,0 +1,27 @@
+interface ClassArgs {
+  auth: string
+}
+interface RunFnInput {
+  input: {
+    image: string,
+    caption?: string
+    question?: string
+  }
+}
+interface RunFnOutput {
+  output: string,
+  status: string
+}
+class MockReplicate {
+  auth: string
+  constructor(auth: string) {
+    this.auth = auth
+  }
+  public run(model: string, input: RunFnInput): RunFnOutput {
+    console.log("running model")
+    return {
+      output: "https://images.unsplash.com/photo-1692290997682-cfd9aadd0fd6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=900&q=60",
+      status: "succeeded"
+    }
+  }
+}
