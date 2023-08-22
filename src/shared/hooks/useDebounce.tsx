5ac15e100effd3c764d2de4e558f6250491442fd
nassorc <macrossan17@gmail.com>
2023-08-22 16:01:10 -0700

    Add debounce hook

---
+++ src/shared/hooks/useDebounce.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export default function useDebounce(value: string, delay: number) {
+  const [debouncedValue, setDebounceValue] = useState(value);
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if(value) {
+      timer = setTimeout(() => {
+        setDebounceValue(debouncedValue);
+      }, delay);
+    }
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [value, delay]);
+
+  return debouncedValue;
+}
